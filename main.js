c879dee812c70189ec4c28e7848f17b4b9f27bff
epicde0 <101399834+epicde0@users.noreply.github.com>
2023-02-14 17:00:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+song1 = "";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+song2 = "";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+
+function preload()
+{
+  song1 = loadSound("music.mp3");
+  song2 = loadSound("music1.mp3");     
+}
+function setup() {
+canvas = createCanvas(600,500);
+ canvas.center();
+
+video = createCapture(VIDEO);
+video.hide();
+}
+
+function draw() {
+    image(video,0,0,600,500)
+}
+
+function gotPoses(results)
+{
+ if(results.length > 0)
+   {
+     console.log(results);
+     leftWristX =results[0].pose.leftWrist.x;
+     leftWristY = results[0].pose.leftWrist.y;
+     console.log("leftWristX="+leftWristX+"LeftWristY="+leftWristY)
+
+     rightWristX= results[0].pose.rightWrist.x;
+     rightWristY = results[0].pose.rightWrist.y;
+    console.log("rightWristX = " + rightWristX +" rightWristY="+rightWristY);
+   }
+}
